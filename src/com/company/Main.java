def6581cba6139549016583bf6ef89badb7c2547
KnutStorvestre <knutmathiasstorvestre@gmail.com>
2021-01-25 18:45:13 +0100

    added for-loop that prints INF112 10 times

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello World");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("INF112");
+        }
     }
 }
